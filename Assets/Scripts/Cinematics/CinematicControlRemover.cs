e1d69b03d3b2c3f0246b1b4bdf93160661745b18
Colin Benson <colin.j.benson@gmail.com>
2021-09-25 09:16:22 -0700

    Removes player control when cutscenes start. Gives back control when they end.

---
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -0,0 +1,35 @@
+﻿using RPG.Control;
+using RPG.Core;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    class CinematicControlRemover: MonoBehaviour
+    {
+        PlayerController playerController;
+        GameObject player;
+        private void Start()
+        {
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+            GetComponent<PlayableDirector>().played += DisableControl;
+
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        private void DisableControl(PlayableDirector pd)
+        {
+            print("Disabled Control");
+
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            playerController = player.GetComponent<PlayerController>();
+            playerController.enabled = false;
+        }
+
+        private void EnableControl(PlayableDirector pd)
+        {
+            print("Enabled Control");
+            playerController.enabled = true;
+        }
+    }
+}
