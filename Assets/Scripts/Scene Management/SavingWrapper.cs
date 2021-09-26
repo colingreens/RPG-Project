a5b3746852441bbbf219801bd0e829c115542dba
Colin Benson <colin.j.benson@gmail.com>
2021-09-26 15:36:58 -0700

    Updated saver to work with multiple properties.

--- Assets/Scripts/Scene Management/SavingWrapper.cs
+++ Assets/Scripts/Scene Management/SavingWrapper.cs
@@ -6,11 +6,16 @@ namespace RPG.SceneManagement
 {
     public class SavingWrapper : MonoBehaviour
     {
+        [SerializeField] float fadeInTime = 0.2f;
+
         const string defaultSaveFile = "save";
 
         private IEnumerator Start()
         {
+            var fader = FindObjectOfType<Fader>();
+            fader.FadeOutImmediate();
             yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+            yield return fader.FadeIn(fadeInTime);
         }
 
         private void Update()
