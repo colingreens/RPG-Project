fa3689d9d9dda09e5e1566862171ea43061c0ce4
Colin Benson <colin.j.benson@gmail.com>
2021-09-27 23:27:59 -0700

    Pickups will respawn now!

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace RPG.Combat
@@ -5,13 +7,31 @@ namespace RPG.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon;
+        [SerializeField] float respawnTime = 5f;
+
         private void OnTriggerEnter(Collider other)
         {
             print("Player has entered pickup");
             if (other.tag == "Player")
             {                
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
+            }
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        private void ShowPickup(bool shouldShow)
+        {
+            GetComponent<Collider>().enabled = shouldShow;
+            foreach (Transform item in transform)
+            {
+                item.gameObject.SetActive(shouldShow);
             }
         }
     }
