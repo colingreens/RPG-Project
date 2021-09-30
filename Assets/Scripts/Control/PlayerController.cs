f2527f1d3d08901e97d0030271c2d89243a7311b
cbenson <cbenson@idtdna.com>
2021-09-30 15:10:42 -0700

    limits length of navmesh distance

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -11,8 +11,9 @@ namespace RPG.Control
 {
     public partial class PlayerController : MonoBehaviour
     {
-        [SerializeField] CursorMapping[] cursorMappings = null;
+        [SerializeField] private CursorMapping[] cursorMappings = null;
         [SerializeField] private float maxNavMeshProjectionDistance = 1f;
+        [SerializeField] private float maxNavPathLength = 40f;
 
         private Fighter fighter;
         private Health health;
@@ -132,9 +133,36 @@ namespace RPG.Control
                 return false;
 
             target = navMeshHit.position;
-            return true;
+
+            NavMeshPath path = new NavMeshPath();
+            bool hasPath = NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
+
+            if (!hasPath)
+                return false;
+
+            if (path.status != NavMeshPathStatus.PathComplete)
+                return false;
+
+            if (GetPathLength(path) > maxNavPathLength)
+                return false;
+
+                return true;
         }
 
+        private float GetPathLength(NavMeshPath path)
+        {
+            var total = 0f;
+
+            if (path.corners.Length < 2)
+                return total;
+
+            for (int i = 0; i < path.corners.Length - 1; i++)
+            {
+                total += Vector3.Distance(path.corners[i], path.corners[i + 1]);
+            }
+
+            return total;
+        }
 
         private CursorMapping GetCursorMapping(CursorType type)
         {
