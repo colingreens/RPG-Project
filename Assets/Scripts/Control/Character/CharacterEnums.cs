42483b6a73cbca9b264721596f475cfea3f35bd0
Colin Benson <colin.j.benson@gmail.com>
2021-10-16 13:01:06 -0700

    Got the camera setup for character

---
+++ Assets/Scripts/Control/Character/CharacterEnums.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace RPG.Control.Character
+{
+    public enum CharacterState
+    {
+        Default,
+    }
+
+    public enum OrientationMethod
+    {
+        TowardsCamera,
+        TowardsMovement,
+    }
+
+    public struct PlayerCharacterInputs
+    {
+        public float MoveAxisForward;
+        public float MoveAxisRight;
+        public Quaternion CameraRotation;
+        public bool JumpDown;
+        public bool CrouchDown;
+        public bool CrouchUp;
+    }
+
+    public struct AICharacterInputs
+    {
+        public Vector3 MoveVector;
+        public Vector3 LookVector;
+    }
+
+    public enum BonusOrientationMethod
+    {
+        None,
+        TowardsGravity,
+        TowardsGroundSlopeAndGravity,
+    }
+}
--- Assets/Scripts/Control/Character/MyCharacterController.cs
+++ Assets/Scripts/Control/Character/MyCharacterController.cs
@@ -1,11 +1,143 @@
-using System.Collections;
+using RPG.Control.Core;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Rpg.Control.Character
+namespace RPG.Control.Character
 {
-    public class MyCharacterController : MonoBehaviour
+    public class MyCharacterController : MonoBehaviour, ICharacterController
     {
-       
+        public KinematicCharacterMotor Motor;
+
+        [Header("Stable Movement")]
+        public float MaxStableMoveSpeed = 10f;
+        public float StableMovementSharpness = 15f;
+        public float OrientationSharpness = 10f;
+        public OrientationMethod OrientationMethod = OrientationMethod.TowardsCamera;
+
+        [Header("Air Movement")]
+        public float MaxAirMoveSpeed = 15f;
+        public float AirAccelerationSpeed = 15f;
+        public float Drag = 0.1f;
+
+        [Header("Jumping")]
+        public bool AllowJumpingWhenSliding = false;
+        public float JumpUpSpeed = 10f;
+        public float JumpScalableForwardSpeed = 10f;
+        public float JumpPreGroundingGraceTime = 0f;
+        public float JumpPostGroundingGraceTime = 0f;
+
+        [Header("Misc")]
+        public List<Collider> IgnoredColliders = new List<Collider>();
+        public BonusOrientationMethod BonusOrientationMethod = BonusOrientationMethod.None;
+        public float BonusOrientationSharpness = 10f;
+        public Vector3 Gravity = new Vector3(0, -30f, 0);
+        public Transform MeshRoot;
+        public Transform CameraFollowPoint;
+        public float CrouchedCapsuleHeight = 1f;
+
+        public CharacterState CurrentCharacterState { get; private set; }
+
+        private Collider[] _probedColliders = new Collider[8];
+        private RaycastHit[] _probedHits = new RaycastHit[8];
+        private Vector3 _moveInputVector;
+        private Vector3 _lookInputVector;
+        private bool _jumpRequested = false;
+        private bool _jumpConsumed = false;
+        private bool _jumpedThisFrame = false;
+        private float _timeSinceJumpRequested = Mathf.Infinity;
+        private float _timeSinceLastAbleToJump = 0f;
+        private Vector3 _internalVelocityAdd = Vector3.zero;
+        private bool _shouldBeCrouching = false;
+        private bool _isCrouching = false;
+
+        private Vector3 lastInnerNormal = Vector3.zero;
+        private Vector3 lastOuterNormal = Vector3.zero;
+
+        private void Awake()
+        {
+            TransitionToState(CharacterState.Default);
+            Motor.CharacterController = this;
+        }
+
+        public void TransitionToState(CharacterState newState)
+        {
+            CharacterState tmpInitialState = CurrentCharacterState;
+            OnStateExit(tmpInitialState, newState);
+            CurrentCharacterState = newState;
+            OnStateEnter(newState, tmpInitialState);
+        }
+
+        public void OnStateEnter(CharacterState state, CharacterState fromState)
+        {
+            switch (state)
+            {
+                case CharacterState.Default:
+                    {
+                        break;
+                    }
+            }
+        }
+
+        public void OnStateExit(CharacterState state, CharacterState toState)
+        {
+            switch (state)
+            {
+                case CharacterState.Default:
+                    {
+                        break;
+                    }
+            }
+        }
+
+        public void AfterCharacterUpdate(float deltaTime)
+        {
+            
+        }
+
+        public void BeforeCharacterUpdate(float deltaTime)
+        {
+           
+        }
+
+        public bool IsColliderValidForCollisions(Collider coll)
+        {
+            return false;
+        }
+
+        public void OnDiscreteCollisionDetected(Collider hitCollider)
+        {
+            
+        }
+
+        public void OnGroundHit(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, ref HitStabilityReport hitStabilityReport)
+        {
+            
+        }
+
+        public void OnMovementHit(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, ref HitStabilityReport hitStabilityReport)
+        {
+            
+        }
+
+        public void PostGroundingUpdate(float deltaTime)
+        {
+            
+        }
+
+        public void ProcessHitStabilityReport(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, Vector3 atCharacterPosition, Quaternion atCharacterRotation, ref HitStabilityReport hitStabilityReport)
+        {
+            
+        }
+
+        public void UpdateRotation(ref Quaternion currentRotation, float deltaTime)
+        {
+            
+        }
+
+        public void UpdateVelocity(ref Vector3 currentVelocity, float deltaTime)
+        {
+            
+        }
+
     }
 }
